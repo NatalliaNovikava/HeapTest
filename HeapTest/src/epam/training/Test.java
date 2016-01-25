4c93797cb2ef1f969e06c59dda5450bdf676544e
Natalia Novikava <Natallia.Novikava>
2016-01-25 12:23:24 +0300

    This is a new commit for what I originally planned to be amended

---
+++ HeapTest/src/epam/training/Test.java
@@ -0,0 +1,21 @@
+package epam.training;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+	//	-Xms1024m
+	//	-Xmx2048m
+	
+		long[] ll;
+		for(int i = 1;; i++)
+		{
+			System.out.println("1");
+			ll = new long[i*20*1024*1024];
+		}
+	}
+
+}
